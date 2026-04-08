d89af9425f85349e19f0087e678d896e9bea7422
Callum <CallumBNeal@gmail.com>
2026-04-08 14:27:44 -0500

    Connected to ftp server it is reading something

--- Assignment3/CALLUM/ftpprogram.go
+++ Assignment3/CALLUM/ftpprogram.go
@@ -2,32 +2,30 @@ package main
 
 import (
 	"fmt"
-	"github.com/goftp/client"
+	"github.com/secsy/goftp"
 
 )
 
-var config = client.Config{
+var config = goftp.Config{
 	User: "anonymous",
 	Password: "",
 }
 
 
 func FtpConnect() {
-	conn, err := client.DialConfig(config, "138.47.99.21")
+	conn, err := goftp.DialConfig(config, "138.47.99.21:21")
 	if err != nil {
 		return
 	}
 
-	defer conn.Quit()
-
-	entries, err := conn.List("/")
+	entries, err := conn.ReadDir("/")
 	if err != nil {
 		return
 	}
 
 	for i := 0; i < len(entries); i++{
 		a := entries[i]
-		fmt.Println(a.name)
+		fmt.Println(a.Name)
 	}
 
 }
