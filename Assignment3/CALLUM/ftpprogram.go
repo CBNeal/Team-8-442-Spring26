de6a84f866695447dac2894ba6915cc75191ea66
Callum <CallumBNeal@gmail.com>
2026-04-09 14:28:29 -0500

    Reads and parses into bin

--- Assignment3/CALLUM/ftpprogram.go
+++ Assignment3/CALLUM/ftpprogram.go
@@ -6,6 +6,13 @@ import (
 
 )
 
+const ( 
+	Address = "138.47.99.21"
+
+	// The Decode number chooses whether it is decoding 7 or 10 bits
+	decodenum = '7'
+)
+
 var config = goftp.Config{
 	User: "anonymous",
 	Password: "",
@@ -13,24 +20,35 @@ var config = goftp.Config{
 
 
 func FtpConnect() {
-	conn, err := goftp.DialConfig(config, "localhost:21")
+	conn, err := goftp.DialConfig(config, Address)
 	if err != nil {
 		return
 	}
 
-	entries, err := conn.ReadDir("/")
+	entries, err := conn.ReadDir("/7")
 	if err != nil {
 		return
 	}
 
 	for i := 0; i < len(entries); i++{
-		a := entries[i]
-		fmt.Println(a.Name())
-		fmt.Printf("%s %s", a.Mode(), a.Name())
+		entry  := entries[i].Mode()
+		
+		fmt.Printf("%s\n", entry)
+		a := entry.String()
+		workingword := ""
+		for k := 3; k < len(a); k++{
+			if a[k] != '-'{
+				workingword += "1"
+			}else{
+				workingword += "0"
+			}
+		}
+		fmt.Printf("%s\n", workingword)
+
+
 	}
 
 }
-
 
 func main() {
 	FtpConnect()
