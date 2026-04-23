8c7cc6d46f98dbc26c99a612262e1fa4fe206b9b
Callum <CallumBNeal@gmail.com>
2026-04-22 20:11:11 -0500

    READS VALUES, ALL INTS AND DOES NOT CALC YET NOT WORKING COPY

--- Assignment4/Callum/chatclient.go
+++ Assignment4/Callum/chatclient.go
@@ -3,20 +3,58 @@ package main
 import (
 	"fmt"
 	"net"
-	
+	"bufio"
 	
 )
+
+const (
+	add = "138.47.99.21:31337")
 
 func main() {
 
 	fmt.Println("Connecting to the chat server ...")
 
-	conn, err := net.Dial("tcp", "138.47.99.21:31337")
+	conn, err := net.Dial("tcp", add)
 	if err != nil {
+		fmt.Println("FAILED TO CONNECT TO SERVER")
 		return
 	}
 	defer conn.Close()
 	
 	fmt.Println("Connected")
 	fmt.Println()
+
+	Scanner := bufio.NewReader(conn)
+	/*
+	var ActualMessage []rune
+	var timdelays []float64
+	var bits []int
+	*/
+	//prev := time.Now()
+
+	for {
+		bit, err := Scanner.ReadByte()
+		if err != nil {
+			return
+		}
+		// This is the actual calc for the time delay
+		/*
+		now := time.Now()
+		delay := now.Sub(prev).Seconds()
+		prev = now
+		*/
+
+
+		char := rune(bit)
+		fmt.Print(char)
+		/*
+		ActualMessage += char
+
+		timdelays = append(timdelay, delay)
+	*/	
+	}
+
+		
+
+
 }
